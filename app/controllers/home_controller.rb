b1f2035ed3e13c4ffc24ed66db5e464e101f22bb
trombini <leandro.ae@gmail.com>
2021-12-06 09:48:47 -0300

    pesquisa adicionada

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
     def index         
         @recipes = Recipe.all
     end    
+
+    def search
+        @recipes = Recipe.where('name like ? OR recipe_type like ?',
+          "%#{params[:q]}%", "%#{params[:q]}%")
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index' 
 
   resources :recipes
-
+  get 'search', to:"home#search"
 end
